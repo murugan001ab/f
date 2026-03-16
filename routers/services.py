d2f612fdc3f3994547684c0c433574ec0364383b
murugan <murugan001ab@gmail.com>
2026-03-16 20:52:09 +0530

    auth check user added

--- routers/services.py
+++ routers/services.py
@@ -1,17 +1,13 @@
-from fastapi import APIRouter, Depends,HTTPException
-
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from db.session import SessionLocal     
+from db.session import SessionLocal
 from models.user import User
-
 
 service = APIRouter(prefix="/services", tags=["Services"])
 
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
     finally:
@@ -20,10 +16,13 @@ def get_db():
 @service.get("/user/{user_id}")
 async def get_user_services(user_id: int, db: Session = Depends(get_db)):
 
-    user = await db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.id == user_id, User.is_verified == True).first()
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    return {"user_id": user.id, "name": user.name, "email": user.email}
-
+    return {
+        "user_id": user.id,
+        "name": user.name,
+        "email": user.email
+    }
